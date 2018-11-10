7002329d8fad066211eec5c24261b781fa419181
Raphael Croce <rcroce7@gmail.com>
2018-11-10 01:07:00 -0600

    Implement basic details component to display people and movie info

---
+++ src/views/Details.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+    console.log("PROPS", this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        DETAILS
+      </div>
+    )
+  }
+}
+
+export default Details;
